7926b4c2d162cb20a01e50f72589d96571d2826e
Divya Mali <162532947+malidivya184@users.noreply.github.com>
2026-04-20 11:07:28 +0530

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -1,5 +1,22 @@
 import boto3
+import os
 
 s3 = boto3.client('s3')
 
-s3.upload_file('index.html', 'bucket-for-file-upload1', 'index.html')
+BUCKET_NAME = 'bucket-for-file-upload1'
+FOLDER_PATH = 'images'   # local folder in repo
+
+def upload_folder():
+    for root, dirs, files in os.walk(FOLDER_PATH):
+        for file in files:
+            local_path = os.path.join(root, file)
+
+            # Create S3 path (keep folder structure)
+            s3_path = os.path.relpath(local_path, FOLDER_PATH)
+
+            print(f"Uploading {local_path} to {s3_path}")
+
+            s3.upload_file(local_path, BUCKET_NAME, f"images/{s3_path}")
+
+if __name__ == "__main__":
+    upload_folder()
